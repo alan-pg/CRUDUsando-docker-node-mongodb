0429cde2fe9929fecacabe2c4ce5f5a22221487f
Alan <alan92goncalves@gemail.com>
2020-09-01 22:04:24 -0300

    AccountController.js refatorado para usar o caminho absoluto e usar getCollection no lugar de getDatabase

--- src/controllers/AccountController.js
+++ src/controllers/AccountController.js
@@ -1,33 +1,30 @@
 import faker from 'faker';
-import { getDatabase  } from '../database/Mongo';
+import { getCollection, COLLECTION_ACCOUNTS  } from 'database/Mongo';
 
 export default {
   list: async (req, res) => {
     try {
-        const fields = {
-            name: true,
-            username: true,
-            email: true,
-        }  
-        const db = await getDatabase();
-        const collection = db.collection('accounts');
-        const data = await collection.find({}).project(fields).toArray();
-        res.json({message: 'Accounts found', data });
-    }catch(e){
-        console.error(e);
-        res.json({message: `Something's wrong`, error: e});
+      const fields = {
+        name: true,
+        username: true,
+        email: true,
+      };
+      const collection = await getCollection(COLLECTION_ACCOUNTS);
+      const data = await collection.find({}).project(fields).toArray();
+      res.json({message: 'Accounts found', data });
+    }catch(e) {
+      console.error(e);
+      res.json({message: `Something's wrong`, error: e});
     }
   },
   create: async (req, res) => {
     try {
-
       const account = faker.helpers.createCard(); 
-      const db = await getDatabase();
-      const collection = db.collection('accounts');
+      const collection = await getCollection(COLLECTION_ACCOUNTS);
       const result = await collection.insertOne(account);
       const data = result.ops[0];
       res.json({message: 'Account Created', data });
-    }catch(e){
+    }catch(e) {
       console.error(e);
       res.json({message: `Something's wrong`, error: e});
     }
